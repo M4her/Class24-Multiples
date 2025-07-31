027ca073de6edd55e8ed8d09eb884209120c55e2
M4her <maher44hmed@gmail.com>
2025-08-01 00:55:41 +0600

    pratice

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 
 let input = document.querySelector("input")
 let result = document.querySelector(".result")
-let numberInput = document.querySelector("#numberInput")
+let h2 = document.querySelector("h2")
+let h3 = document.querySelector("h3")
 
 
-let handleButton = ()=>{
-    result.innerHTML = ''
-    if(!input.value){
-        alert('Insert a value')
-    }else{
-        for (let i = 1 ; i < 11 ; i++){
-            
-            result.innerHTML += input.value + ' x ' + i + ' = ' + input.value*i +'<br>'
+
+    let handleButton =()=>{
+        result.innerHTML = ''
+        if(!input.value){
+            h3.style = "display:block"
+        h3.innerHTML = 'Please insert a value'
+        }else{
+            for(let i = 1 ; i<11 ; i++){
+                result.innerHTML += input.value + 'x' + i + '=' + i*input.value + '<br>'
+            }
+            input.value=''
         }
+        
 
     }
-        numberInput.value = ''
 
-
-}
     
